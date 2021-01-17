76e3b610435baa964beae4a639a97b85ba025cbe
haru22 <y.a.m.aa0867@gmail.com>
2021-01-17 13:33:59 +0900

    added PostVC

---
+++ HackDavis/Model/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  HackDavis
+//
+//  Created by Haruna Yamakawa on 1/17/21.
+//
+
+import Foundation
+
+struct Post {
+    let foodName: String
+    let foodType: String
+    let spice: String
+    let description: String
+}
